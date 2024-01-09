93ef83dcfc8c73ca37a140dea993c118fd1ddd73
dkgoutham <d.goutham@iitg.ac.in>
2024-01-09 21:49:56 +0530

    some more front-end done

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,23 +1,47 @@
 import QuizProgress from "@/components/QuizProgress";
 import Timer from "@/components/Timer";
 import type { NextPage } from "next";
+import Button from "@/components/Button";
 
 const Quiz: NextPage = () => {
     return (
-        <div className=" flex flex-col items-center px-[13vw] py-[10vh]">
+        <div className=" flex flex-col items-center px-[10vw] py-[10vh]">
             <QuizProgress/>
-            <div className="relative bg-gray w-[65vw] h-[60vh] rounded-3xl px-[15%]">
+            <div className="relative bg-gray w-[55vw] rounded-3xl px-[10%] pt-16 pb-[3%] flex flex-col items-center text-white">
                 <Timer/>
-                <div>
-                    
+                <div className="font-bold text-2xl">
+                    Question #1
                 </div>
 
-                <div>
-
+                <div className="text-center font-normal my-[6%] text-lg">
+                    Which of the following Indian States / Union Territories does not have a land boundary with any foreign countries?
                 </div>
 
-                <div>
-
+                <div className="grid grid-cols-2 gap-x-6 gap-y-3 font-extralight">
+                    <Button
+                        option="A. Coal"
+                        optionWidth="15vw"
+                        optionHeight="8vh"
+                        optionFontWeight="400"
+                    />
+                    <Button
+                        option="B. Natural Gas"
+                        optionWidth="15vw"
+                        optionHeight="8vh"
+                        optionFontWeight="400"
+                    />
+                    <Button
+                        option="C. Nuclear Energy"
+                        optionWidth="15vw"
+                        optionHeight="8vh"
+                        optionFontWeight="400"
+                    />
+                    <Button
+                        option="D. Petroleum"
+                        optionWidth="15vw"
+                        optionHeight="8vh"
+                        optionFontWeight="400"
+                    />
                 </div>
             </div>
             
