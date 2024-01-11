3d90c7cf0a922244d6c03e6c60ad3ea425ee3ba5
dkgoutham <d.goutham@iitg.ac.in>
2024-01-11 17:10:05 +0530

    added google signin

---
+++ src/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,60 @@
+import prisma from "../../../../prisma/index";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth/next";
+import { signIn } from "next-auth/react";
+import {session} from "../../../utils/session"
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+
+const authOption: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    providers:[
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks:{
+        async signIn({account, profile}){
+            if (!profile?.email) {
+                throw new Error('No profile')
+              }
+        
+              await prisma.user.upsert({
+                where: {
+                  email: profile.email,
+                },
+                create: {
+                  email: profile.email,
+                  name: profile.name,
+                },
+                update: {
+                  name: profile.name,
+                },
+              })
+              return true
+        },
+        session,
+        async jwt({ token, user, account, profile }) {
+            if (profile) {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: profile.email,
+                    },
+                })
+                if (!user) {
+                throw new Error('No user found')
+                }
+                token.id = user.id
+            }
+            return token
+        },
+    }
+}
+
+const handler = NextAuth(authOption)
+export { handler as GET, handler as POST }
