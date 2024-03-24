097140d11b134c4d60a2e09ba3ed591c657a15c4
AtriChatterjee1 <print.atri@gmail.com>
2024-03-25 00:06:56 +0530

    added timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,23 @@
-import type { NextPage } from "next";
+import { NextPage } from 'next';
+import { useState, useEffect } from 'react';
 
 const Timer: NextPage = () => {
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSecondsRemaining(prevSeconds => Math.max(0, prevSeconds - 1)); // Ensure value stays non-negative
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="absolute w-20 h-20 rounded-full border-2 border-goldenrod bg-questionMarkGray text-white text-3xl font-bold flex justify-center items-center top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        100
+      {secondsRemaining}
     </div>
   );
 };
 
 export default Timer;
+
