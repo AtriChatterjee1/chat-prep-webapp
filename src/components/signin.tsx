a116bf40154442f634413444ea5e7822833b076e
AtriChatterjee1 <print.atri@gmail.com>
2024-03-27 17:23:57 +0530

    abhi just random token generate krke login kara rhe , integrate krna bacha hai

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -22,7 +22,7 @@ const provider = new GoogleAuthProvider();
 const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost:3000/',
+    url: 'http://localhost:3000/quiz',
     // This must be true.
     handleCodeInApp: true,
   };
